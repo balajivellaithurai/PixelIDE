76cbeb0d9055c100ade245726a4af4d629be14cf
balajivellaithurai <balajivellaithurai@gmail.com>
2026-07-23 03:50:52 +0530

    fix: render FileTabs above EditorPanel in Workspace layout

--- client/src/pages/Workspace.jsx
+++ client/src/pages/Workspace.jsx
@@ -1,21 +1,25 @@
 import Navbar from "../components/layout/Navbar";
 import Sidebar from "../components/layout/Sidebar";
+import FileTabs from "../components/layout/FileTabs";
 import EditorPanel from "../components/layout/EditorPanel";
 import Console from "../components/layout/Console";
 
 const Workspace = () => {
-    return (
-        <div className="h-screen flex flex-col bg-[#111827]">
-            <Navbar />
+  return (
+    <div className="h-screen flex flex-col bg-[#111827]">
+      <Navbar />
 
-            <div className="flex flex-1">
-                <Sidebar />
-                <EditorPanel />
-            </div>
-
-            <Console />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+          <FileTabs />
+          <EditorPanel />
         </div>
-    );
+      </div>
+
+      <Console />
+    </div>
+  );
 };
 
 export default Workspace;
